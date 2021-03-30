769bcab3a01d3ca0680486aaa6671620f6185f9e
skdksldk1485 <skdksldk1485@naver.com>
2021-03-31 00:54:29 +0900

    Expressjs의 static file 경로 설정 추가(/upload)

--- server/app.js
+++ server/app.js
@@ -21,6 +21,8 @@ app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/upload', uploadRoutes);
 
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 
 // catch 404 and forward to error handler
