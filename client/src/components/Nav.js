9f286b6990fe5fecb72593c4cd83ff86f01d98ba
skdksldk1485 <skdksldk1485@naver.com>
2021-06-05 04:12:58 +0900

    반응형 헤더(모바일용) 적용

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -4,44 +4,44 @@ import { NavLink } from 'react-router-dom';
 const Nav = () => {
 
   return (
-    <header>
-      <nav className='nav'>
-      <div>
-        <ul className='nav__list'>
-          <li className='nav__list__item'>
-            <NavLink exact to='/shop' activeClassName='active'>
-              ALL
-            </NavLink>
-          </li>
-          <li className='nav__list__item'>
-            <NavLink to='/shop/category/outers' activeClassName='active'>
-              OUTERS
-            </NavLink>
-          </li>
-          <li className='nav__list__item'>
-            <NavLink to='/shop/category/tops' activeClassName='active'>
-              TOPS
-            </NavLink>
-          </li>
-          <li className='nav__list__item'>
-            <NavLink to='/shop/category/pants' activeClassName='active'>
-              PANTS
-            </NavLink>
-          </li>
-          <li className='nav__list__item'>
-            <NavLink to='/shop/category/dresses' activeClassName='active'>
-              DRESSES
-            </NavLink>
-          </li>
-          <li className='nav__list__item'>
-            <NavLink to='/shop/category/caps' activeClassName='active'>
-              CAPS
-            </NavLink>
-          </li>
-        </ul>
-      </div>
+    <>
+      <nav className='side'>
+        <div>
+          <ul className='side__list'>
+            <li className='side__list__item'>
+              <NavLink exact to='/shop' activeClassName='active'>
+                ALL
+              </NavLink>
+            </li>
+            <li className='side__list__item'>
+              <NavLink to='/shop/category/outers' activeClassName='active'>
+                OUTERS
+              </NavLink>
+            </li>
+            <li className='side__list__item'>
+              <NavLink to='/shop/category/tops' activeClassName='active'>
+                TOPS
+              </NavLink>
+            </li>
+            <li className='side__list__item'>
+              <NavLink to='/shop/category/pants' activeClassName='active'>
+                PANTS
+              </NavLink>
+            </li>
+            <li className='side__list__item'>
+              <NavLink to='/shop/category/dresses' activeClassName='active'>
+                DRESSES
+              </NavLink>
+            </li>
+            <li className='side__list__item'>
+              <NavLink to='/shop/category/caps' activeClassName='active'>
+                CAPS
+              </NavLink>
+            </li>
+          </ul>
+        </div>
       </nav>
-    </header>
+    </>
   );
 };
 
